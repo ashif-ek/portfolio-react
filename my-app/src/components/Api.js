856051029606629945c2e72084e01c9aa702a641
Ashif EK <ashifek11@gmail.com>
2025-11-01 00:13:15 +0530

    end point fix

--- my-app/src/components/Api.js
+++ my-app/src/components/Api.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-
-
-const Api = axios.create({
-  baseURL: "http://localhost:5000/",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export default Api
-
-
-
-
-
 // import axios from "axios";
 
 
 // const Api = axios.create({
-//   baseURL: "https://portfolio-server-35.onrender.com",
+//   baseURL: "http://localhost:5000/",
 //   headers: {
 //     "Content-Type": "application/json",
 //   },
 // });
 
 // export default Api
+
+
+
+
+
+import axios from "axios";
+
+
+const Api = axios.create({
+  baseURL: "https://portfolio-server-35.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default Api
