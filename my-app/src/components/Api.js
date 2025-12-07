db19664353ce52ea065f9eff20addd433dd1a91b
Ashif EK <ashifek11@gmail.com>
2025-12-07 15:45:39 +0530

    feat: Add Axios-based API client configuration with base URL and headers.

--- my-app/src/components/Api.js
+++ my-app/src/components/Api.js
@@ -1,27 +1,18 @@
-// import axios from "axios";
-
-
-// const Api = axios.create({
-//   baseURL: "http://localhost:5000/",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
-
-// export default Api
-
-
-
-
-
 import axios from "axios";
 
-
 const Api = axios.create({
-  baseURL: "https://portfolio-server-35.onrender.com",
+  baseURL: "http://localhost:5000/",
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export default Api
+export default Api;
+
+// Production URL (Uncomment when deploying)
+// const Api = axios.create({
+//   baseURL: "https://portfolio-server-35.onrender.com",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
